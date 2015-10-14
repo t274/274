8bb98032f32d6103382cae7ac8e5cb1ca1808f9d
nozdormal <thomas.tetz@gmail.com>
2015-10-13 20:18:24 -0600

    testing after comments and formatting

--- Assignment01/Part02/pow_mod/pow_mod.cpp
+++ Assignment01/Part02/pow_mod/pow_mod.cpp
@@ -25,39 +25,33 @@ uint32_t generate_private(){
     return private_key;
 }
 
-/*  */
+/* Prevents multiplication overflow by using modular arithmetic */
 uint32_t mul_mod(uint32_t a, uint32_t b, uint32_t m) {
-  uint32_t result = 0;
-  uint32_t x = b % m;
+    uint32_t result = 0;
+    uint32_t x = b % m;
    
-  for (int i = 0; i < 32; ++i) {
-    uint32_t myBit = 31 - i;
-    //~ Serial.print(i);
-    //~ Serial.print(": ");
-    //~ Serial.println(((a << myBit) >> 31));
-    if (((a << myBit) >> 31) == 1) {
-      result = (result + x) % m;
+    for (int i = 0; i < 32; ++i) {
+        uint32_t myBit = 31 - i;
+        if (((a << myBit) >> 31) == 1) {
+            result = (result + x) % m;
+        }
+        x = (x << 1) % m;
     }
-    x = (x << 1) % m;
-  }
-
-  return result;
+    return result;
 }
 
+/* Calculates base^power mod m, improved version */
 uint32_t pow_mod(uint32_t base, uint32_t power, uint32_t m) {
-  uint32_t result = 1;
-  uint32_t x = base % m;
-  for (int i = 0; i < 32; ++i ) {
-    if ((power & (1UL << i)) != 0) {
-      result = mul_mod(result, x, m);
+    uint32_t result = 1;
+    uint32_t x = base % m;
+    for (int i = 0; i < 32; ++i ) {
+        if ((power & (1UL << i)) != 0) {
+            result = mul_mod(result, x, m);
+        }
+        x = mul_mod(x, x, m);
     }
-    x = mul_mod(x, x, m);
-  }
-  return result;
+    return result;
 }
-
-
-
 
 /** Waits for a certain number of bytes on Serial3 or timeout 
  * @param nbytes: the number of bytes we want
@@ -67,69 +61,74 @@ uint32_t pow_mod(uint32_t base, uint32_t power, uint32_t m) {
  * @return True if the required number of bytes have arrived.
  */
 bool wait_on_serial3( uint8_t nbytes, long timeout ) {
-  unsigned long deadline = millis() + timeout;//wraparound not a problem
-  while (Serial3.available()<nbytes && (timeout<0 || millis()<deadline)) 
-  {
-    delay(1); // be nice, no busy loop
-  }
-  return Serial3.available()>=nbytes;
+    unsigned long deadline = millis() + timeout;//wraparound not a problem
+    while (Serial3.available()<nbytes && (timeout<0 || millis()<deadline)) 
+    {
+        delay(1); // be nice, no busy loop
+    }
+    return Serial3.available()>=nbytes;
 }
 
 /** Writes an uint32_t to Serial3, starting from the least-significant
  * and finishing with the most significant byte. 
  */
 void uint32_to_serial3(uint32_t num) {
-  Serial3.write((char) (num >> 0));
-  Serial3.write((char) (num >> 8));
-  Serial3.write((char) (num >> 16));
-  Serial3.write((char) (num >> 24));
+    Serial3.write((char) (num >> 0));
+    Serial3.write((char) (num >> 8));
+    Serial3.write((char) (num >> 16));
+    Serial3.write((char) (num >> 24));
 }
 
 /** Reads an uint32_t from Serial3, starting from the least-significant
  * and finishing with the most significant byte. 
  */
 uint32_t uint32_from_serial3() {
-  uint32_t num = 0;
-  num = num | ((uint32_t) Serial3.read()) << 0;
-  num = num | ((uint32_t) Serial3.read()) << 8;
-  num = num | ((uint32_t) Serial3.read()) << 16;
-  num = num | ((uint32_t) Serial3.read()) << 24;
-  return num;
+    uint32_t num = 0;
+    num = num | ((uint32_t) Serial3.read()) << 0;
+    num = num | ((uint32_t) Serial3.read()) << 8;
+    num = num | ((uint32_t) Serial3.read()) << 16;
+    num = num | ((uint32_t) Serial3.read()) << 24;
+    return num;
 }
 
-
+/* Initializing skey and ckey for future use */
 uint32_t skey = 0;
 uint32_t ckey = 0;
 
-
-bool handshake(int mode){
+/* State machine for inter-arduino handshake.
+ * Exchanges public keys to properly calculate the shared key.
+ */
+void handshake(int mode){
+    /* creating a clear naming system for the states */
     typedef enum { Start=0, WaitForAck_1, Listen, WaitingForKey_1,
         WaitForAck_2, WaitingForKey_2, WaitForAck_3,
          DataExchange } State;
     char* StateNames[] = {"Start","WaitForAck_1","Listen",
         "WaitingForKey_1","WaitForAck_2","WaitingForKey_2",
         "WaitForAck_3","DataExchange" };
+        
     State state = State(mode);
-    unsigned long time_check = millis();
     bool logic_check = 0;
     
+    /* continues until DataExchange is reached */
     while( (state != DataExchange) ){
 
         logic_check = 0;
+        
+        //print statements of the states are included but commented out
         
         /* client starting point */
         if (state == Start){
             Serial3.write('C');
             uint32_to_serial3(ckey);
             state = WaitForAck_1;
-            Serial.println("Start");
+            //~ Serial.println("Start");
         }
         else if (state == WaitForAck_1){
             logic_check = wait_on_serial3(5,1000);
-            Serial.println("WaitForAck_1");
+            //~ Serial.println("WaitForAck_1");
                 if(logic_check &&  ((char)(Serial3.read()) == 'A') ){
                 skey = uint32_from_serial3();
-                //Serial.print();
                 Serial3.write('A');
                 state = DataExchange;
             }
@@ -139,7 +138,7 @@ bool handshake(int mode){
         /* server starting point */
         else if (state == Listen){
             logic_check = wait_on_serial3(1,1000);
-            Serial.println("Listen");
+            //~ Serial.println("Listen");
             if(logic_check && (((char)Serial3.read()) == 'C')){
                 state = WaitingForKey_1;
             }
@@ -147,7 +146,7 @@ bool handshake(int mode){
         }
         else if (state == WaitingForKey_1){
             logic_check = wait_on_serial3(4,1000);
-            Serial.println("WaitingForKey_1");
+            //~ Serial.println("WaitingForKey_1");
             if(logic_check){
                 ckey = uint32_from_serial3();
                 Serial3.write('A');
@@ -158,7 +157,7 @@ bool handshake(int mode){
         }
         else if (state == WaitForAck_2){
             logic_check = wait_on_serial3(1,1000);
-            Serial.println("WaitForAck_2");
+            //~ Serial.println("WaitForAck_2");
             
             char WaitForAck_2_char = ((char)Serial3.read());
 
@@ -170,14 +169,13 @@ bool handshake(int mode){
                 else if(WaitForAck_2_char == 'C'){
                     state = WaitingForKey_2;
                 }
-                else { Serial.println("please help");}
             }
             else {state = Listen;}
         }
         else if (state == WaitingForKey_2){
-            Serial.println("WaitingForKey_2");
+            //~ Serial.println("WaitingForKey_2");
             logic_check = wait_on_serial3(4,1000);
-            Serial.print("  logic_check: "); Serial.println(logic_check);
+            //~ Serial.print("  logic_check: "); Serial.println(logic_check);
             
             if(logic_check){
                 state = WaitForAck_3;
@@ -187,7 +185,7 @@ bool handshake(int mode){
         else if (state == WaitForAck_3){
             logic_check = wait_on_serial3(1,1000);
             
-            Serial.println("WaitForAck_3");
+            //~ Serial.println("WaitForAck_3");
             char WaitForAck_3_char = ((char)Serial3.read());
             if(logic_check){
                 if(WaitForAck_3_char == 'C'){
@@ -201,9 +199,7 @@ bool handshake(int mode){
         }
         
     }
-    //Serial.print("End state: "); Serial.println(StateNames[state]);
-    Serial.println("DataExchange");
-    return (state == DataExchange); //useless
+    //~ Serial.println("DataExchange");
 }
 
 
@@ -220,14 +216,14 @@ bool handshake(int mode){
  * http://www.firstpr.com.au/dsp/rand31/rand31-park-miller-carta.cc.txt
  */
 uint32_t next_key(uint32_t current_key) {
-  const uint32_t modulus = 0x7FFFFFFF; // 2^31-1
-  const uint32_t consta = 48271; // we use that this is <=15 bits
-  uint32_t lo = consta*(current_key & 0xFFFF);  
-  uint32_t hi = consta*(current_key >> 16); 
-  lo += (hi & 0x7FFF)<<16;
-  lo += hi>>15;
-  if (lo > modulus) lo -= modulus;
-  return lo;
+    const uint32_t modulus = 0x7FFFFFFF; // 2^31-1
+    const uint32_t consta = 48271; // we use that this is <=15 bits
+    uint32_t lo = consta*(current_key & 0xFFFF);  
+    uint32_t hi = consta*(current_key >> 16); 
+    lo += (hi & 0x7FFF)<<16;
+    lo += hi>>15;
+    if (lo > modulus) lo -= modulus;
+    return lo;
 }
 
 /* Encrypts outgoing bytes */
@@ -255,15 +251,14 @@ uint32_t decrypt(uint32_t incoming_byte, uint32_t shared_key_1){
     return shared_key_1;
 }
 
+/* Unnecessary but could have additional features in the future */
 int get_configuration(){
     int mode = digitalRead(13);
     return mode;
 }
 
 int main(void) {
-    
-    //THIS IS THE RIGHT MAIN
-    
+
     // Initialise Arduino functionality
     init();
   
@@ -271,26 +266,7 @@ int main(void) {
     #ifdef USBCON
         USBDevice.attach();
     #endif
-    
-    /* remember to change values and data types
-     * 
-     * modify pow_mod with mul_mod
-     * 
-     * reorder to have beginning stuff at top?
-     * 
-     * configure
-     * generate private key (32 bits)
-     *     ckey if client, skey if server
-     *     still display public keys on monitor
-     * handshake
-     * generate shared
-     * 
-     * begin communication?
-     *     use new encryption method
-     *     must have synchronized next key on encrypt and decrypt
-     * 
-     */
-    
+
      /* SETUP */
     uint32_t prime = 2147483647;
     uint32_t generator = 16807;
@@ -298,42 +274,30 @@ int main(void) {
     
     pinMode(13, INPUT);
     
-    bool diditwork = 0;
-    // this should be done after private key made
-    // gets configuration of the arduino
-    // made a function in case more complication methods are used
     Serial.begin(9600);
-    Serial3.begin(9600);
-    int mode = get_configuration();
-    Serial.print("mode: "); Serial.println(mode);
-    //5V means server
+    Serial3.begin(9600); // Serial3: communication with other board
     
+    int mode = get_configuration(); //5V means server
+    Serial.print("mode: "); Serial.println(mode);
+    
+    Serial.println("Generating private key. Please wait...");
+    // calculates the random 32 bit private key
     do{
-        //@TODO: is there a better way?
         private_key = generate_private();
     }while(private_key >= prime);
     Serial.print("private_key: "); Serial.println(private_key);
     
-    
-    //handshake before this
-    //depending on configuration do dif
-    
-    //~ if(digitalRead(13)==HIGH){
-        skey = pow_mod(generator, private_key, prime);
-        //~ Serial.print("skey: "); Serial.println(skey);
-    //~ }
-    //~ else{ 
-        ckey = pow_mod(generator, private_key, prime); 
-        //~ Serial.print("ckey: "); Serial.println(ckey);
-    //~ }
 
+    /* both arduinos calculate both because using if-statements
+     * caused problems for unknown reasons
+     */
+    skey = pow_mod(generator, private_key, prime);
+    ckey = pow_mod(generator, private_key, prime); 
 
-    
     uint32_t shared_key_1 = 0, shared_key_2 = 0;
-    
 
-    if(mode){diditwork = handshake(2);}
-    else {diditwork = handshake(0);}
+    if(mode){ handshake(2); }
+    else    { handshake(0); }
     
     if(digitalRead(13)==HIGH){
         Serial.print("ckey: "); Serial.println(ckey);
@@ -345,52 +309,30 @@ int main(void) {
         Serial.print("skey: "); Serial.println(skey);
         Serial.print("Client private_key: "); Serial.println(private_key);
         shared_key_1 = pow_mod(skey, private_key, prime);
+        shared_key_2 = shared_key_1;
     }
-    
-
     Serial.print("The shared key is: "); Serial.println(shared_key_1);
     
-    //~ if (mode == 0) { diditwork = handshake(0); } //check which it should be
-    //~ else           { diditwork = handshake(9999); }
-    //~ 
-    
-    
-    //~ /* Generates private key, sometimes takes a while */
-    //~ while(private_key >= prime){
-        //~ //@TODO: is there a better way?
-        //~ private_key = generate_private();
-    //~ }
-    //~ 
-    
-    /* Generates private key, sometimes takes a while */
-    
-    
     /* LOOP */
-     // Serial3: communication with other board
     int incoming_byte = 0;
     int my_PC_byte = 0;
     while(true){
         
-        //grab byte from other Arduino
+        //grabs byte from other Arduino and decrypts it
         if(Serial3.available()){
-            //decrypt byte
             incoming_byte = Serial3.read();
             shared_key_1 = decrypt(incoming_byte, shared_key_1);
-
         }
         
-        //grab byte from PC
-
+        //grab byte from PC and encrypts it
         if(Serial.available()){
-            //encrypt byte
             my_PC_byte = Serial.read();
             shared_key_2 = encrypt(my_PC_byte, shared_key_2);
-
         }
     }
+    
     Serial.end();
     Serial3.end();
-    //Add your custom initialization here
 
     return 0;
 }
